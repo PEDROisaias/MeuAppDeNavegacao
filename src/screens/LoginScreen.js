f7279e5b4ee475400d134aeac28b6d2084f794e9
PEDROisaias <pedrogomes123md@gmail.com>
2025-05-21 10:13:11 -0300

    Terminar login

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,1 +1,25 @@
 import React from 'react';
+import Animated, {useSharedValue, withTiming, useAnimatedStyle, Easing, } from 'react-native-reanimated';
+import { View, StyleSheet, TextInput, Button, Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+
+export default function LoginScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <View style={styles.containerLogin}>
+                <TextInput style={styles.inpEmail}> Insira seu email</TextInput>
+                        <Button 
+                        title="Entrar"
+                        onPress={() => navigation.navigate('Home')}
+                        ></Button>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+})
